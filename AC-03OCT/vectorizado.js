091222b7ec5ca22465fa54242fb9de47bee2937a
MarRuiz1888 <marruiz1888@gmail.com>
2024-10-03 16:20:55 -0500

    Update vectorizado.js

--- AC-03OCT/vectorizado.js
+++ AC-03OCT/vectorizado.js
@@ -1,35 +1,106 @@
-const puntos = [
-    new Punto(100, 100),
-    new Punto(200, 100),
-    new Punto(250, 200),
-    new Punto(150, 250),
-    new Punto(50, 200)
-];
+class Punto {
+    #x;
+    #y;
 
-function dibujarPoligono() {
-    const svgContainer = document.getElementById('svgContainer');
-    let svg = `<svg width="400" height="400">`;
+    constructor(x, y) {
+        this.#x = x;
+        this.#y = y;
+    }
 
-    svg += `<polygon points="`;
+    getX() {
+        return this.#x;
+    }
 
-    puntos.forEach(punto => {
-        svg += `${punto.getX()},${punto.getY()} `;
-    });
+    getY() {
+        return this.#y;
+    }
 
-    svg += `" style="fill:lightblue;stroke:black;stroke-width:2" />`;
-    svg += `</svg>`;
+    setX(x) {
+        this.#x = x;
+    }
 
-    svgContainer.innerHTML = svg;
+    setY(y) {
+        this.#y = y;
+    }
 }
 
-function calcularCentroide() {
+const puntos = [];
+
+function PuntosAleatorios(n) {
+    for (let i = 0; i < n; i++) {
+        const x = Math.random() * 600;
+        const y = Math.random() * 600;
+        puntos.push(new Punto(x, y));
+    }
+}
+
+function CruceLineas() {
+    const puntoBase = puntos.reduce((min, punto) => {
+        if (punto.getY() < min.getY() || (punto.getY() === min.getY() && punto.getX() < min.getX())) {
+            return punto;
+        }
+        return min;
+    });
+
+    puntos.sort((a, b) => {
+        const anguloA = Math.atan2(a.getY() - puntoBase.getY(), a.getX() - puntoBase.getX());
+        const anguloB = Math.atan2(b.getY() - puntoBase.getY(), b.getX() - puntoBase.getX());
+        return anguloA - anguloB;
+    });
+}
+
+function Dibujar() {
+    const svg = document.getElementById('svgCanvas');
+    svg.innerHTML = ''; 
+
+    const polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+    const points = puntos.map(p => `${p.getX()},${p.getY()}`).join(' '); 
+    polygon.setAttribute('points', points);
+    polygon.setAttribute('fill', 'lightblue');
+    polygon.setAttribute('stroke', 'black');
+
+    svg.appendChild(polygon);
+}
+
+function Centroide() {
     let xTotal = 0, yTotal = 0;
+
     puntos.forEach(punto => {
         xTotal += punto.getX();
         yTotal += punto.getY();
     });
+
     const centroide = new Punto(xTotal / puntos.length, yTotal / puntos.length);
     return centroide;
 }
 
-dibujarPoligono();
+let mostrarCentroide = false;
+
+function toggleCentroide() {
+    mostrarCentroide = !mostrarCentroide; 
+    Dibujar(); 
+
+    if (mostrarCentroide) {
+        const svg = document.getElementById('svgCanvas');
+        const centroide = Centroide();
+
+        
+        puntos.forEach(punto => {
+            const linea = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            linea.setAttribute('x1', centroide.getX());
+            linea.setAttribute('y1', centroide.getY());
+            linea.setAttribute('x2', punto.getX());
+            linea.setAttribute('y2', punto.getY());
+            linea.setAttribute('stroke', 'purple');
+            svg.appendChild(linea);
+        });
+
+        
+        const circulo = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circulo.setAttribute('cx', centroide.getX());
+        circulo.setAttribute('cy', centroide.getY());
+        circulo.setAttribute('r', 5);
+        circulo.setAttribute('fill', 'red');
+        svg.appendChild(circulo);
+    }
+}
